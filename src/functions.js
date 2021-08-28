761aa79d018a966f9103fbbf173acfd1eeadd310
Localhostov <localhostov@gmail.com>
2021-08-28 23:59:59 +0300

    moving functions to another file

---
+++ src/functions.js
@@ -0,0 +1,46 @@
+const padZero = (str, len) => {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+};
+
+export const invertColor = (hex, bw) => {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    var r = parseInt(hex.slice(0, 2), 16),
+        g = parseInt(hex.slice(2, 4), 16),
+        b = parseInt(hex.slice(4, 6), 16);
+    if (bw) {
+        return (r * 0.299 + g * 0.587 + b * 0.114) > 150
+            ? '#000000'
+            : '#FFFFFF';
+    }
+    r = (255 - r).toString(16);
+    g = (255 - g).toString(16);
+    b = (255 - b).toString(16);
+    return "#" + padZero(r) + padZero(g) + padZero(b);
+};
+
+export const rgbaToHexa = rgba => {
+    var a,
+    rgb = rgba.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i),
+    alpha = (rgb && rgb[4] || "").trim(),
+    hex = rgb ?
+    (rgb[1] | 1 << 8).toString(16).slice(1) +
+    (rgb[2] | 1 << 8).toString(16).slice(1) +
+    (rgb[3] | 1 << 8).toString(16).slice(1) : rgba;
+  
+    if (alpha !== "") { a = alpha; }
+    else { a = '01'; }
+    hex = hex + a;
+  
+    return `#` + hex;
+}
