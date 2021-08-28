22fc699011b1d4369f3a827bcaab39a2fb35c56c
Localhostov <localhostov@gmail.com>
2021-08-29 00:03:41 +0300

    adding texts, adding another picker, changing background and header colors when changing color from picker

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import { SketchPicker } from 'react-color';
+import { ChromePicker } from 'react-color';
 
 import { 
 	Panel, 
@@ -14,19 +14,29 @@ import {
 import { Icon24PaletteOutline, Icon24Copy } from '@vkontakte/icons';
 
 import './Home.css';
+import {
+	rgbaToHexa,
+	invertColor
+} from '../functions';
 
 const texts = [
-	`Практический опыт показывает, что повышение уровня гражданского сознания играет важную роль в формировании дальнейших направлений развитая системы массового участия.`,
-	`Повседневная практика показывает, что постоянное информационно-техническое обеспечение нашей деятельности напрямую зависит от новых предложений.`,
+	`_Практический_ опыт показывает, _что повышение уровня_ гражданского сознания играет важную роль в формировании дальнейших направлений развитая системы массового участия.`,
+	`Повседневная практика _показывает, что постоянное информационно-техническое_ обеспечение нашей деятельности напрямую зависит от новых предложений.`,
 	`Равным образом дальнейшее развитие различных форм деятельности играет важную роль в формировании экономической целесообразности принимаемых решений.`,
-	`Значимость этих проблем настолько очевидна, что начало повседневной работы по формированию позиции позволяет выполнить важнейшие задания по разработке ключевых компонентов...`,
-	`Значимость этих проблем настолько очевидна, что консультация с профессионалами из IT требует определения и уточнения модели развития.`,
-	`С другой стороны социально-экономическое развитие требует определения и уточнения всесторонне сбалансированных нововведений.`,
-	`Таким образом, дальнейшее развитие различных форм деятельности обеспечивает широкому кругу специалистов участие в формировании позиций, занимаемых участниками в отношении поставленных...`,
-	`Задача организации, в особенности же постоянное информационно-техническое обеспечение нашей деятельности напрямую зависит от ключевых компонентов планируемого обновления?`,
-	`Дорогие друзья, рамки и место обучения кадров требует от нас системного анализа дальнейших направлений развития проекта!`,
-	`Соображения высшего порядка, а также выбранный нами инновационный путь представляет собой интересный эксперимент проверки ключевых компонентов планируемого обновления.`,
-	`С другой стороны выбранный нами инновационный путь требует определения и уточнения новых предложений!`,
+	`Значимость этих проблем настолько очевидна, _что начало повседневной_ работы по формированию позиции позволяет выполнить важнейшие задания по разработке ключевых компонентов...`,
+	`Значимость этих проблем настолько очевидна, что консультация с _профессионалами из IT_ требует определения и уточнения модели развития.`,
+	`С другой стороны _социально-экономическое развитие требует_ определения и уточнения всесторонне сбалансированных нововведений.`,
+	`Таким образом, дальнейшее развитие различных _форм деятельности обеспечивает_ широкому кругу специалистов участие в формировании позиций, занимаемых участниками в отношении поставленных...`,
+	`Задача организации, в _особенности же постоянное информационно-техническое_ обеспечение нашей деятельности напрямую зависит от ключевых компонентов планируемого обновления?`,
+	`Дорогие друзья, рамки _и место обучения кадров требует_ от нас системного анализа дальнейших направлений развития проекта!`,
+	`Соображения высшего порядка, а также выбранный нами _инновационный путь представляет_ собой интересный эксперимент проверки ключевых компонентов планируемого обновления.`,
+	`С другой стороны выбранный нами инновационный путь _требует определения и уточнения_ новых предложений!`,
+	`Задача организации, в особенности же новая модель организационной деятельности _создаёт предпосылки качественно новых шагов для..._`,
+	`Равным образом рамки и место обучения кадров _обеспечивает широкому кругу специалистов_ участие в формировании системы...`,
+	`Практический опыт показывает, что _выбранный нами инновационный путь требует от нас_ системного анализа ключевых компонентов...`,
+	`Разнообразный и богатый опыт постоянный количественный _рост и сфера нашей активности влечет за_ собой процесс...`,
+	`Практический опыт показывает, что выбранный нами инновационный путь _способствует подготовке и реализации существующих финансовых и..._`,
+	`Задача организации, в особенности же социально-экономическое развитие позволяет оценить _значение направлений прогрессивного развития_...`
 ];
 
 const Home = ({ id }) => {
@@ -37,8 +47,26 @@ const Home = ({ id }) => {
 		rgba: 'rgba(144, 19, 254, 1)'
 	});
 	const [ tooltip, setTooltip ] = useState('');
+	const [ lorem, setLorem ] = useState('');
 
-	const isPhone = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+	const regex = {
+		paragraph: /(?:\r\n){2,}/g,
+		formatting: /(\_.*?\_)|(\*.*?\*)/g,
+	};
+
+	let applyFormatting = (text) => {
+		return text.split(regex.formatting).filter(n => n).map((str) => {
+		let parsed = str[0] == '_'
+			? (<b>{applyFormatting(str.substr(1, str.length - 2))}</b>)
+			: str;
+		return parsed;
+	  });
+	};
+
+	useEffect(() => {
+		setLorem(texts[Math.floor(Math.random() * texts.length)]);
+		applyFormatting(lorem);
+	}, []);
 
 	const copyColor = from => {
 		switch (from) {
@@ -63,54 +91,12 @@ const Home = ({ id }) => {
 		}
 	};
 
-	const padZero = (str, len) => {
-		len = len || 2;
-		var zeros = new Array(len).join('0');
-		return (zeros + str).slice(-len);
-	};
-
-	const invertColor = (hex, bw) => {
-		if (hex.indexOf('#') === 0) {
-			hex = hex.slice(1);
-		}
-		
-		if (hex.length === 3) {
-			hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-		}
-		if (hex.length !== 6) {
-			throw new Error('Invalid HEX color.');
-		}
-		var r = parseInt(hex.slice(0, 2), 16),
-			g = parseInt(hex.slice(2, 4), 16),
-			b = parseInt(hex.slice(4, 6), 16);
-		if (bw) {
-			return (r * 0.299 + g * 0.587 + b * 0.114) > 186
-				? '#000000'
-				: '#FFFFFF';
-		}
-		r = (255 - r).toString(16);
-		g = (255 - g).toString(16);
-		b = (255 - b).toString(16);
-		return "#" + padZero(r) + padZero(g) + padZero(b);
-	};
-
-	const rgbaToHexa = rgba => {
-		var a,
-		rgb = rgba.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i),
-		alpha = (rgb && rgb[4] || "").trim(),
-		hex = rgb ?
-		(rgb[1] | 1 << 8).toString(16).slice(1) +
-		(rgb[2] | 1 << 8).toString(16).slice(1) +
-		(rgb[3] | 1 << 8).toString(16).slice(1) : rgba;
-	  
-		if (alpha !== "") { a = alpha; }
-		else { a = '01'; }
-		hex = hex + a;
-	  
-		return `#` + hex;
-	}
-
 	const convertColor = color => {
+		document.documentElement.style.setProperty('--background_content', color.hex);
+		document.documentElement.style.setProperty('--header_background', color.hex);
+		document.documentElement.style.setProperty('--header_text', invertColor(color.hex, true));
+		document.documentElement.style.setProperty('--header_tint', invertColor(color.hex, true));
+
 		setColors({
 			hex: (color.hex).toString().toUpperCase(),
 			hexa: rgbaToHexa(`rgba(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}, ${color.rgb.a})`).toString().toUpperCase(),
@@ -118,6 +104,19 @@ const Home = ({ id }) => {
 			rgba: `rgba(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}, ${color.rgb.a})`
 		});
 	};
+
+	let formattedText;
+
+	formattedText = lorem.split(regex.paragraph) || [];
+	formattedText = formattedText.map((text, i) => {
+		return (
+		<div key={i}>
+			{
+				applyFormatting(text)
+			}
+		</div>
+		)
+	});
 
 	return (
 		<Panel id={id}>
@@ -129,10 +128,10 @@ const Home = ({ id }) => {
 			</PanelHeader>
 			<br/>
 			<center>
-				<SketchPicker
+				<ChromePicker
 				color={colors.hex}
-				onChangeComplete={e => convertColor(e)}
-				width={isPhone ? '80%' : '50%'}
+				onChange={e => convertColor(e)}
+				width='80%'
 				/>
 			</center>
 			<center>
@@ -141,7 +140,7 @@ const Home = ({ id }) => {
 				>
 					{tooltip 
 					? <div style={{ color: colors.hex }}>{tooltip}</div> 
-					: 'Выберите цвет'}
+					: <div style={{ color: invertColor(colors.hex, true) }}>Выберите цвет</div>}
 				</h3>
 			</center>
 
@@ -202,7 +201,11 @@ const Home = ({ id }) => {
 			<Div
 			style={{ color: colors.hex }}
 			>
-				{texts[Math.floor(Math.random() * texts.length)]}
+				<Card style={{ background: invertColor(colors.hex, true), borderRadius: 10, padding: 10 }}>
+					{
+						formattedText
+					}
+				</Card>
 			</Div>
 		</Panel>
 	)
